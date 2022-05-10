f9db967858a76aecf13eefffb0597b6f68f70a3c
kuklkulkul <57491397+kuklkulkul@users.noreply.github.com>
2022-05-10 22:03:14 +0900

    dekita

--- typical90/001_Yokan_Party.c
+++ typical90/001_Yokan_Party.c
@@ -4,11 +4,15 @@
 #define MAX_N   (100000)
 #define MAX_INPT    (1000000000)
 
+typedef enum {
+    false_,
+    true_,
+} Bool_;
+
 int32_t A_arr[(MAX_N + 2U)];
 
-
 int32_t calc_min(int32_t slct_num, int32_t max_n, int32_t uppr);
-int32_t part_cal(int32_t strt, int32_t slct_num, int32_t max_n, int32_t lower);
+Bool_ part_cal(int32_t slct_num, int32_t max_n, int32_t lower);
 
 int main(void)
 {
@@ -43,105 +47,78 @@ int main(void)
 int32_t calc_min(int32_t slct_num, int32_t max_n, int32_t uppr)
 {
     int32_t ret;
-    int32_t i, j;
     int32_t srch_val_l = 1;
     int32_t srch_val_u = uppr;
-    int32_t srch_val_m = uppr / 2;
-    int32_t tmp_u, tmp_l, tmp_m;
+    int32_t srch_val_m = (srch_val_l + srch_val_u + 1) / 2;
+    Bool_ tmp_m;
 
-    ret = part_cal(0, slct_num, max_n, srch_val_l);
-
-    while (srch_val_l != srch_val_u)
+    while ((srch_val_l < srch_val_m) && (srch_val_m < srch_val_u))
     {
-        tmp_u = part_cal(0, slct_num, max_n, srch_val_u);
+        tmp_m = part_cal(slct_num, max_n, srch_val_m);
         
-        
-        if (tmp_u != -1)
+        if (tmp_m == false_)
         {
-            ret = tmp_u;
-            break;
+            srch_val_u = srch_val_m;      
         }
         else
         {
-            tmp_m = part_cal(0, slct_num, max_n, srch_val_m);
-
-            if (tmp_m != -1)
-            {
-                srch_val_l = srch_val_m;
-                srch_val_u = (srch_val_l + srch_val_u) / 2;
-            }
-            else
-            {
-                srch_val_u = srch_val_m - 1;
-            }
+            srch_val_l = srch_val_m;
         }
+        
+        srch_val_m = (srch_val_l + srch_val_u + 1) / 2;
+    } 
 
+    tmp_m = part_cal(slct_num, max_n, srch_val_u);
+
+    if (tmp_m == false_)
+    {
+        ret = srch_val_l;
+    }
+    else
+    {
+        ret = srch_val_u;
     }
 
     return ret;
 }
 
-int32_t part_cal(int32_t strt, int32_t slct_num, int32_t max_n, int32_t lower)
+Bool_ part_cal(int32_t slct_num, int32_t max_n, int32_t lower)
 {
-    int32_t ret = -1;
-    int32_t i;
+    Bool_ ret = false_;
+    int32_t i = max_n;
     int32_t tmp;
+    int32_t start = A_arr[0];
+    int32_t end = A_arr[(max_n + 1)];
+    int32_t *p_slct = &A_arr[1];
+    int32_t slct = slct_num;
 
-    if (slct_num == 1)
+    while (i >= 1)
     {
-        for(i = (strt + 1); i <= max_n; i++)
+        if (lower <= (*p_slct - start))
         {
-            tmp = A_arr[i] - A_arr[strt];
-            if (tmp >= lower)
+            start = *p_slct;
+            slct--;
+
+            if (slct <= 0)
             {
-                int32_t tmp_l = A_arr[(max_n + 1)] - A_arr[i];
-
-                if (tmp_l >= lower)
-                {
-                    if (tmp > tmp_l)
-                    {
-                        ret = tmp_l;
-                    }
-                    else
-                    {
-                        ret = tmp;
-                    }
-                }
                 break;
-            }
-            else{/*nothing*/}
+            }else{/*nothing*/}
         }
-    }
-    else
-    {
-        for(i = (strt + 1); i <= (max_n - slct_num + 1); i++)
-        {
-            tmp = A_arr[i] - A_arr[strt];
+        else{/*nothing*/}
 
-            if (tmp >= lower)
-            {
-                ret = tmp;
-                tmp = part_cal(i, (slct_num - 1), max_n, lower);
-
-                if (tmp == -1)
-                {
-                    ret = -1;
-                }
-                else if (ret > tmp)
-                {
-                    ret = tmp;
-                }
-                else
-                {
-                    /* nothing */
-                }
-
-                break;
-            }
-            else{/*nothing*/}
-        }
+        p_slct++;
+        i--;
     }
 
+    if ((slct <= 0))
+    {
+        if (lower <= (end - *p_slct))
+        {
+            ret = true_;
+        }
+        else{/*nothing*/}
+    }
+    else{/*nothing*/}
 
     return ret;
 }
