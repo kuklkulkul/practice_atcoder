e224236f8831672938129bf490b58f989465994d
kuklkulkul <57491397+kuklkulkul@users.noreply.github.com>
2022-05-19 23:37:53 +0900

    解けるけどTLE

--- typical90/005_Restricted_Digits.c
+++ typical90/005_Restricted_Digits.c
@@ -2,31 +2,96 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define MOD_NUM (1000000007U)
 #define MAX_N (1000000000000000000UL)
-#define MAX_N (1000)
-#define DE_QUE_FAIL ((uint32_t)(-1))
-
-enum {NO_CHK, CHKD};
-enum {FAIL_ENQUE = -1, SUCS_ENQUE};
 
 int main(void)
 {
-    uint32_t i;
+    uint32_t i, j;
+    uint64_t k;
     uint64_t inpt_N;
-    uint32_t inpt_B, inpt_K;
-    uint32_t *c_arr;
+    uint32_t inpt_B, inpt_K, inpt_C;
+    uint32_t mod_10_B;
+    uint32_t tmp_mod, tmp_cal;
+    uint32_t *mod_arr;
+    uint32_t *tmp_mod_arr;
+    uint32_t *mod_base_arr;
+    uint32_t *tmp_mod_base_arr;
     size_t size_uint32_t = sizeof(uint32_t);
 
     /* get input */
     (void)scanf("%lu%u%u\n", &inpt_N, &inpt_B, &inpt_K);
 
-    c_arr = (uint32_t *)malloc(size_uint32_t * (size_t)(inpt_K + 1U));
+    mod_10_B = 10U % inpt_B;
 
-    for (i = 1U; i <= inpt_K; i++)
+    mod_arr = (uint32_t *)malloc(size_uint32_t * (size_t)inpt_B);
+    mod_base_arr = (uint32_t *)malloc(size_uint32_t * (size_t)inpt_B);
+    tmp_mod_arr = (uint32_t *)malloc(size_uint32_t * (size_t)inpt_B);
+    tmp_mod_base_arr = (uint32_t *)malloc(size_uint32_t * (size_t)inpt_B);
+
+    for (i = 0U; i < inpt_B; i++)
     {
-        (void)scanf("%u", &c_arr[i]);
+        mod_arr[i] = 0U;
+        mod_base_arr[i] = 0U;
+        tmp_mod_arr[i] = 0U;
+        tmp_mod_base_arr[i] = 0U;
+    }
+
+    for (i = 0U; i < inpt_K; i++)
+    {
+        (void)scanf("%u", &inpt_C);
+        inpt_C %= inpt_B;
+        mod_arr[inpt_C]++;
+        mod_base_arr[inpt_C]++;
     }
     (void)scanf("\n");
+
+    for (k = 1UL; k < inpt_N; k++)
+    {
+        /* 次の桁のMODを求める */
+        for (i = 0U; i < inpt_B; i++)
+        {
+            if (mod_base_arr[i])
+            {
+                tmp_mod = (i * mod_10_B) % inpt_B;
+                tmp_mod_base_arr[tmp_mod] = mod_base_arr[i];
+            }
+            else{/*nothing*/}
+        }
+        
+        for (i = 0U; i < inpt_B; i++)
+        {
+            mod_base_arr[i] = tmp_mod_base_arr[i];
+            tmp_mod_base_arr[i] = 0U;
+        }
+
+        /* 次の桁のMODを求める */
+        for (i = 0U; i < inpt_B; i++)
+        {
+            if (mod_base_arr[i])
+            {
+                for (j = 0U; j < inpt_B; j++)
+                {
+                    if (mod_arr[j])
+                    {
+                        tmp_cal = (i + j) % inpt_B;
+                        tmp_mod_arr[tmp_cal] += mod_arr[j];
+                        tmp_mod_arr[tmp_cal] %= MOD_NUM;
+                    }
+                    else{/*nothing*/}
+                }
+            }
+            else{/*nothing*/}
+        }
+
+        for (i = 0U; i < inpt_B; i++)
+        {
+            mod_arr[i] = tmp_mod_arr[i];
+            tmp_mod_arr[i] = 0U;
+        }
+    }
+
+    printf("%u\n", mod_arr[0]);
 
     return 0;
 }
